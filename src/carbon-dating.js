9dccf5347ce7ca875066a7af74100c5255dd971c
maksimdedush <79816522+maksimdedush@users.noreply.github.com>
2021-03-15 22:19:57 +0300

    Add files via upload

---
+++ src/carbon-dating.js
@@ -0,0 +1,13 @@
+
+const CustomError = require();
+
+const MODERN_ACTIVITY= 15; 
+const HALF_LIFE_PERIOD= 5730;
+
+module.exports = function dateSample(sampleActivity) {
+  if((typeof(sampleActivity)!='string') || (15 < +sampleActivity) || 
+    (+sampleActivity < 1) || (isNaN(parseFloat(sampleActivity))) ) return false;
+
+  else return (Math.ceil(Math.log((MODERN_ACTIVITY / parseFloat(sampleActivity))) / (0.693 / HALF_LIFE_PERIOD)));
+
+};
---
+++ src/hanoi-tower.js
@@ -0,0 +1,11 @@
+
+  module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    let turns = Math.pow(2, disksNumber) - 1;
+    let speed = turnsSpeed / 3600;
+    let sec = Math.floor(turns / speed);
+  
+    return {
+      turns: turns,
+      seconds: sec,
+    };
+  };
