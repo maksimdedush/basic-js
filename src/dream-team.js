cdc900c94bd805e9b85636128b702aad133140f3
maksimdedush <79816522+maksimdedush@users.noreply.github.com>
2021-03-15 22:27:42 +0300

    Delete src directory

--- src/dream-team.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = function createDreamTeam(members) {
-
-    if(Array.isArray(members)) {
-      return members.filter(member => typeof member === 'string').map(item => 
-        item.trim().slice(0, 1).toUpperCase()).sort().join('');
-      } else return false;
-  };
--- src/recursive-depth.js
+++
@@ -1,9 +0,0 @@
-
-  module.exports = class DepthCalculator {
-    calculateDepth(arr, depth=1, levels=[]) {
-      levels.push(depth);
-      arr.forEach(item => {if (Array.isArray(item)) this.calculateDepth(item, depth+1, levels)});
-  
-      return Math.max(...levels);
-    }
-  };
